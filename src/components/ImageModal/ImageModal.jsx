0b12fae4163e3c81a57a8f951446f919bf36f81a
Austin Miller <vadim.sukhetskii@gmail.com>
2024-07-24 22:10:08 +0200

    fixed modal window

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Modal from "react-modal";
 import "./ImageModal.modules.css";
 
-const ImageModal = ({ isOpen, onRequestClose, image }) => (
-  <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-    <img src={image.urls.regular} alt={image.alt_description} />
-    <p>Author: {image.user.name}</p>
-    <p>Likes: {image.likes}</p>
-    <button onClick={onRequestClose}>Close</button>
-  </Modal>
-);
+const ImageModal = ({ isOpen, onRequestClose, image }) => {
+  if (!image) return null;
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Image Modal"
+      className="ImageModal"
+      overlayClassName="ImageModalOverlay"
+    >
+      <img src={image.urls.regular} alt={image.alt_description} />
+    </Modal>
+  );
+};
 
 export default ImageModal;
